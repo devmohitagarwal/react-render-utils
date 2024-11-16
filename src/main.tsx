7af4c6f2e9ded6a9779f53ecffb7fe5b02183709
Mohit Agarwal <37167207+devmohitagarwal@users.noreply.github.com>
2024-11-16 15:25:54 +0000

    fix: tried another approach from main

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,7 @@
 export * from "./When";
 export * from "./Switch";
 export * from "./Range";
+
+export { default as When } from "./When";
+export { default as Switch } from "./Switch";
+export { default as Range } from "./Range";
