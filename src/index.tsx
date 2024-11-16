f4da0f6f216ebce03fabca966624a80bd007fec8
Mohit Agarwal <37167207+devmohitagarwal@users.noreply.github.com>
2024-11-16 23:56:40 +0000

    fix: tried another approach from main

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-export * from "./When/index";
-export * from "./Switch/Switch";
-export * from "./Range/Range";
+export * from "./When/When.js";
+export * from "./Switch/Switch.js";
+export * from "./Range/Range.js";
 
-export { default as When } from "./When/When";
-export { default as Switch } from "./Switch/Switch";
-export { default as Range } from "./Range/Range";
+export { default as When } from "./When/When.js";
+export { default as Switch } from "./Switch/Switch.js";
+export { default as Range } from "./Range/Range.js";
